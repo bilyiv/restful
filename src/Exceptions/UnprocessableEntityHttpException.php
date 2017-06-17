9f33e24e3607c5b42c41ebc96ed5f53701a780fe
belyj_vladislav <beliyvladislav@gmail.com>
2017-06-18 01:27:23 +0300

    create unprocessable entity http exception class

---
+++ src/Exceptions/UnprocessableEntityHttpException.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Bilyiv\Restful\Exceptions;
+
+/**
+ * Class UnprocessableEntityHttpException
+ *
+ * @package Bilyiv\Restful\Exceptions
+ * @author Vladyslav Bilyi <beliyvladislav@gmail.com>
+ */
+class UnprocessableEntityHttpException extends HttpException
+{
+    /**
+     * Constructor.
+     *
+     * @param string     $message  The internal exception message
+     * @param \Exception $previous The previous exception
+     * @param int        $code     The internal exception code
+     */
+    public function __construct($message = null, \Exception $previous = null, $code = 0)
+    {
+        parent::__construct(422, $message, $previous, array(), $code);
+    }
+}
