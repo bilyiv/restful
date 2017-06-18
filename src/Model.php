f01824012962c87e582e9b41a9de7346ce8ee3da
belyj_vladislav <beliyvladislav@gmail.com>
2017-06-18 16:21:36 +0300

    remove functions from model and use created traits

--- src/Model.php
+++ src/Model.php
@@ -2,10 +2,12 @@
 
 namespace Bilyiv\Restful;
 
-use Illuminate\Database\Eloquent\{
-    Builder,
-    Model as BaseModel
+use Bilyiv\Restful\Traits\Eloquent\{
+    EmbedAttributes,
+    FilterAttributes,
+    SortAttributes
 };
+use Illuminate\Database\Eloquent\Model as BaseModel;
 
 /**
  * Class Model
@@ -15,103 +17,5 @@ use Illuminate\Database\Eloquent\{
  */
 class Model extends BaseModel
 {
-    /**
-     * The attributes that are filterable.
-     *
-     * @var array
-     */
-    protected $filterable = [];
-
-    /**
-     * The attributes that are sortable.
-     *
-     * @var array
-     */
-    protected $sortable = [];
-
-    /**
-     * The attributes that are embeddable.
-     *
-     * @var array
-     */
-    protected $embeddable = [];
-
-    /**
-     * Scope a query to filter models.
-     *
-     * @param Builder $query
-     * @param array $params
-     * @return Builder
-     */
-    public function scopeFilter($query, array $params): Builder
-    {
-        $filterAttributes = $this->filterableFromArray($params);
-
-        return $query->where($filterAttributes);
-    }
-
-    /**
-     * Scope a query to sort models.
-     *
-     * @param Builder $query
-     * @param array $params
-     * @return Builder
-     */
-    public function scopeSort($query, array $params): Builder
-    {
-        $sortAttributes = $this->sortableFromArray($params);
-
-        foreach ($sortAttributes as $column => $direction) {
-            $query->orderBy($column, $direction);
-        }
-
-        return $query;
-    }
-
-    /**
-     * Scope a query to embed models.
-     *
-     * @param Builder $query
-     * @param array $params
-     * @return Builder
-     */
-    public function scopeEmbed($query, array $params): Builder
-    {
-        $embedAttributes = $this->embeddableFromArray($params);
-
-        return $query->with($embedAttributes);
-    }
-
-    /**
-     * Get the filterable attributes of a given array.
-     *
-     * @param  array $attributes
-     * @return array
-     */
-    protected function filterableFromArray(array $attributes)
-    {
-        return array_intersect_key($attributes, $this->filterable);
-    }
-
-    /**
-     * Get the sortable attributes of a given array.
-     *
-     * @param  array $attributes
-     * @return array
-     */
-    protected function sortableFromArray(array $attributes)
-    {
-        return array_intersect_key($attributes, $this->sortable);
-    }
-
-    /**
-     * Get the embeddable attributes of a given array.
-     *
-     * @param array $attributes
-     * @return array
-     */
-    protected function embeddableFromArray(array $attributes)
-    {
-        return array_intersect($attributes, $this->embeddable);
-    }
+    use FilterAttributes, SortAttributes, EmbedAttributes;
 }
