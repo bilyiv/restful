7d6f4741113e50ebb397d1b9d0d0327c2de1cc8a
belyj_vladislav <beliyvladislav@gmail.com>
2017-06-18 21:25:20 +0300

    add test for unprocessable entity exception

---
+++ tests/Exceptions/UnprocessableEntityHttpExceptionTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Bilyiv\Restful\Tests\Exceptions;
+
+use Bilyiv\Restful\Exceptions\UnprocessableEntityHttpException;
+use Tests\TestCase;
+
+/**
+ * Class UnprocessableEntityHttpException
+ *
+ * @package Bilyiv\Restful\Tests\Exceptions
+ * @author Vladyslav Bilyi <beliyvladislav@gmail.com>
+ */
+class UnprocessableEntityHttpExceptionTest extends TestCase
+{
+    /**
+     * @var UnprocessableEntityHttpException
+     */
+    protected $exception;
+
+    /**
+     * UnprocessableEntityHttpExceptionTest constructor.
+     */
+    public function __construct()
+    {
+        $this->exception = new UnprocessableEntityHttpException('Test');
+    }
+
+    /**
+     * Test exception status code.
+     */
+    public function testExceptionStatusCode()
+    {
+        $this->assertEquals($this->exception->getStatusCode(), 422);
+    }
+
+    /**
+     * Test exception message.
+     */
+    public function testExceptionMessage()
+    {
+        $this->assertEquals($this->exception->getMessage(), 'Test');
+    }
+}
