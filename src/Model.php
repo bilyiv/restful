6b0c47683bab9e60ed4d7b4e0bb5a927e59f05d6
belyj_vladislav <beliyvladislav@gmail.com>
2017-06-18 19:22:10 +0300

    add scope paginate with params that includes filter, sort, embed scopes

--- src/Model.php
+++ src/Model.php
@@ -7,7 +7,9 @@ use Bilyiv\Restful\Traits\Eloquent\{
     FilterAttributes,
     SortAttributes
 };
+use Illuminate\Contracts\Pagination\Paginator;
 use Illuminate\Database\Eloquent\Model as BaseModel;
+use Illuminate\Database\Eloquent\Builder;
 
 /**
  * Class Model
@@ -18,4 +20,35 @@ use Illuminate\Database\Eloquent\Model as BaseModel;
 class Model extends BaseModel
 {
     use FilterAttributes, SortAttributes, EmbedAttributes;
+
+    /**
+     * Number of items per page.
+     *
+     * @var int
+     */
+    protected $limit = 100;
+
+    /**
+     * Scope a query to paginate models both with with filter, sort and embed scopes.
+     *
+     * @param Builder $query
+     * @param array $params
+     * @return Paginator
+     */
+    public function scopePaginateWithParams($query, array $params)
+    {
+        return $query->scopes([
+                'filter' => [
+                    $params['filter'] ?? []
+                ],
+                'sort' => [
+                    $params['sort'] ?? []
+                ],
+                'embed' => [
+                    $params['embed'] ?? []
+                ]
+            ])
+            ->simplePaginate($params['limit'] ?? $this->limit)
+            ->appends($params);
+    }
 }
